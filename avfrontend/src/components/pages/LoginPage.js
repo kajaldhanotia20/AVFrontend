e6da76383ac927fe3f97d06c5acb5a8642222703
kajaldhanotia20 <kajaldhanotia1997@gmail.com>
2022-04-24 01:15:10 -0700

    adding popup

--- avfrontend/src/components/pages/LoginPage.js
+++ avfrontend/src/components/pages/LoginPage.js
@@ -1,31 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import '../../App.css'
+import "../../App.css";
 
-export default function SignInPage() {
-    return (
-        <div className="text-center m-5-auto">
-            <h2>Sign in</h2>
-            <form action="/home">
-                <p>
-                    <label>Username or email address</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
-                <p>
-                    <label>Password</label>
-                    <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
-                    <br/>
-                    <input type="password" name="password" required />
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Login</button>
-                </p>
-            </form>
-            <footer>
-                <p>First time? <Link to="/register">Create an account</Link>.</p>
-                <p><Link to="/">Back to Homepage</Link>.</p>
-            </footer>
-        </div>
-    )
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function onSubmit(e) {
+    e.preventDefault();
+    alert(`welcome ${username}`);
+    window.location.href = "/home";
+  }
+  return (
+    <div className="text-center m-5-auto">
+      <h2>Sign in</h2>
+      <form onSubmit={onSubmit}>
+        <p>
+          <label>Username or email address</label>
+          <br />
+          <input
+            type="text"
+            name="username"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </p>
+        <p>
+          <label>Password</label>
+          <Link to="/forget-password">
+            <label className="right-label">Forget password?</label>
+          </Link>
+          <br />
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </p>
+        <p>
+          <button id="sub_btn" type="submit">
+            Login
+          </button>
+        </p>
+      </form>
+      <footer>
+        <p>
+          First time? <Link to="/register">Create an account</Link>.
+        </p>
+        <p>
+          <Link to="/">Back to Homepage</Link>.
+        </p>
+      </footer>
+    </div>
+  );
 }
--- avfrontend/src/components/pages/RegisterPage.js
+++ avfrontend/src/components/pages/RegisterPage.js
@@ -1,69 +1,101 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
 
-import '../../App.css'
+import "../../App.css";
 
-export default function SignUpPage() {
+export default function RegisterPage() {
+    const [name, setName] = useState("");
+     function onSubmit(e) {
+       e.preventDefault();
+       alert(`welcome ${name}`);
+       window.location.href = "/login";
+     }
+  return (
+    <div className="text-center m-5-auto">
+      <h2>Join us</h2>
+      <h5>Create your account</h5>
+      <form onSubmit={onSubmit}>
+        <p>
+          <label>First Name</label>
+          <br />
+          <input
+            type="text"
+            name="first_name"
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </p>
 
-    return (
-        <div className="text-center m-5-auto">
-            <h2>Join us</h2>
-            <h5>Create your account</h5>
-            <form action="/home">
+        <p>
+          <label>Last Name</label>
+          <br />
+          <input type="text" name="last_name" required />
+        </p>
+        <p>
+          <label>Username</label>
+          <br />
+          <input type="text" name="username" required />
+        </p>
+        <p>
+          <label>Email address</label>
+          <br />
+          <input type="email" name="email" required />
+        </p>
+        <p>
+          <label>DOB</label>
+          <br />
+          <input type="date" name="dob" required />
+        </p>
+        <p>
+          <label>Phone</label>
+          <br />
+          <input type="number" name="phone" required />
+        </p>
+        <p>
+          <label>Address</label>
+          <br />
+          <input type="text" name="address" required />
+        </p>
+        <p>
+          <label>User Type</label>
+          <br />
 
-                <p>
-                    <label>First Name</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
+          <select name="selectList" id="selectList">
+              <option value="option 1">AV User</option> {" "}
+            <option value="option 2">AV Owner</option>
+          </select>
+        </p>
 
-                <p>
-                    <label>Last Name</label><br/>
-                    <input type="text" name="last_name" required />
-                </p>
-                <p>
-                    <label>Username</label><br/>
-                    <input type="text" name="username" required />
-                </p>
-                <p>
-                    <label>Email address</label><br/>
-                    <input type="email" name="email" required />
-                </p>
-                <p>
-                    <label>DOB</label><br/>
-                    <input type="date" name="dob" required />
-                </p>
-                <p>
-                    <label>Phone</label><br/>
-                    <input type="number" name="phone" required />
-                </p>
-                <p>
-                    <label>Address</label><br/>
-                    <input type="text" name="address" required />
-                </p>
-                <p>
-                    <label>User Type</label><br/>
-                    
-                    <select name="selectList" id="selectList">
-                    <option value="option 1">AV User</option>
-                     <option value="option 2">AV Owner</option>
-                </select>
-                </p>
-
-                <p>
-                    <label>Password</label><br/>
-                    <input type="password" name="password" requiredc />
-                </p>
-                <p>
-                    <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href="https://google.com" target="_blank" rel="noopener noreferrer">terms of service</a></span>.
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Register</button>
-                </p>
-            </form>
-            <footer>
-                <p><Link to="/">Back to Homepage</Link></p>
-            </footer>
-        </div>
-    )
-
+        <p>
+          <label>Password</label>
+          <br />
+          <input type="password" name="password" requiredc />
+        </p>
+        <p>
+          <input type="checkbox" name="checkbox" id="checkbox" required />{" "}
+          <span>
+            I agree all statements in{" "}
+            <a
+              href="https://google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              terms of service
+            </a>
+          </span>
+          .
+        </p>
+        <p>
+          <button id="sub_btn" type="submit">
+            Register
+          </button>
+        </p>
+      </form>
+      <footer>
+        <p>
+          <Link to="/">Back to Homepage</Link>
+        </p>
+      </footer>
+    </div>
+  );
 }
