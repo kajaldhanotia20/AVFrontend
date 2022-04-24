500433a8bf442e3a3c94561f449f0208dd21814b
kajaldhanotia20 <kajaldhanotia1997@gmail.com>
2022-04-24 00:04:20 -0700

    minor

--- avfrontend/src/components/pages/HomePage.js
+++ avfrontend/src/components/pages/HomePage.js
@@ -18,7 +18,7 @@ return(
     <Link to="/ScheduleRide"><Button className="primary-button">Schedule a Ride</Button></Link>
     <Link to="/ViewRideHistory"><Button className="primary-button">View Ride History</Button></Link>
     <Link to="/BillingDashboard"><Button className="primary-button">Billing Dashboard</Button></Link>
-    <Link to="/SensoInfo"><Button className="primary-button">AV Sensor Information</Button></Link>
+    <Link to="/SensorInfo"><Button className="primary-button">AV Sensor Information</Button></Link>
   </Card.Body>
   <Card.Footer className="text-muted">Visible only to AV Users</Card.Footer>
 </Card>
@@ -46,8 +46,8 @@ return(
     <Card.Text>
       With supporting text below as a natural lead-in to additional content.
     </Card.Text>
-    <Link to="/AVData"><Button className="primary-button">See User Data</Button></Link>
-    <Link to="/UserData"><Button className="primary-button">See AV Data</Button></Link>
+    <Link to="/UserData"><Button className="primary-button">See User Data</Button></Link>
+    <Link to="/AVData"><Button className="primary-button">See AV Data</Button></Link>
     
   </Card.Body>
   <Card.Footer className="text-muted">Visible only to AV Admins</Card.Footer>
