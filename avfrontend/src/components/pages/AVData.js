c6ba6eaf83941e622fc8d6052523456dbf0eb377
kajaldhanotia20 <kajaldhanotia1997@gmail.com>
2022-04-24 20:08:57 -0700

    adding auth0

--- avfrontend/src/components/pages/AVData.js
+++ avfrontend/src/components/pages/AVData.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import React from 'react';
+import { Icon, Label, Menu, Table } from 'semantic-ui-react';
 import Card from "react-bootstrap/Card";
 import { CardGroup, Row, Col} from 'react-bootstrap';
 
