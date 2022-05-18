495f0788b7be33858a0b501ba90f8d882cd65bbb
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-05-17 17:20:16 -0700

    autho

--- avfrontend/src/index.js
+++ avfrontend/src/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
- function onRedirectCallback() {
-   window.location.href = '/SensorData';
- }
+//  function onRedirectCallback() {
+//    window.location.href = '/SensorData';
+//  }
 
 ReactDOM.render(
-    
-  <Auth0Provider
-    domain="dev-bmeujak9.us.auth0.com"
-    clientId="BEwNWwZ0kduhIY0ym4PUn3UCXdLvS2na"
-    redirectUri={window.location.origin}
-    onRedirectCallback={onRedirectCallback}
-  >
-    <App />
-  </Auth0Provider>,
+  // <Auth0Provider
+  //   domain="dev-bmeujak9.us.auth0.com"
+  //   clientId="BEwNWwZ0kduhIY0ym4PUn3UCXdLvS2na"
+  //   redirectUri={window.location.origin}
+  //   onRedirectCallback={onRedirectCallback}
+  // >
+  <App />,
   document.getElementById("root")
 );
