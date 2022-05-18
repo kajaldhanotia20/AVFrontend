1cf61a73ba136d688905d781f5601b612c8516f5
Sai Kiran <saikiran.madupu@gmail.com>
2022-05-17 22:36:24 -0700

    Update AdminDashboard.js

--- avfrontend/src/components/pages/AdminDashboard.js
+++ avfrontend/src/components/pages/AdminDashboard.js
@@ -11,15 +11,6 @@ const AdminDashboard = () => {
     return (
         
         <div className="text-center">
-            <h1 className="main-para">Admin Dashboard</h1>
-            
-                <Link to="/UserData"><button className="primary-button">See User Data</button></Link>
-                
-           
-
-            <Link to="/AVData">
-                <button className="primary-button">See AV Data</button>
-            </Link>
 
         </div>
 
